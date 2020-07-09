2dc84d6ba8432cf9910e2e2a41b3b74924466bd9
Leonid Shatilo <shatiloleonid@gmail.com>
2020-07-09 15:13:54 +0300

    lesson_01

--- lesson_01/src/HelloUsername.java
+++ lesson_01/src/HelloUsername.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class HelloUsername {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input your name: ");
-        String name = input.next();
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + args[0] + "!");
     }
 }
